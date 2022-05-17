140543a1ba64cfefa3f4ee07498688132c70df0e
Yifu Wang <200110832@stu.hit.edu.cn>
2022-05-17 16:02:51 +0800

    fixed some bug

--- app/src/main/java/com/hit/sz/application/ImageManager.java
+++ app/src/main/java/com/hit/sz/application/ImageManager.java
@@ -1,10 +1,7 @@
 package com.hit.sz.application;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
-import com.hit.sz.R;
 import com.hit.sz.item.aircraft.BossEnemy;
 import com.hit.sz.item.aircraft.EliteEnemy;
 import com.hit.sz.item.aircraft.HeroAircraft;
@@ -15,11 +12,8 @@ import com.hit.sz.item.prop.BloodProp;
 import com.hit.sz.item.prop.BombProp;
 import com.hit.sz.item.prop.FireProp;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 /**
  * 综合管理图片的加载，访问
  * 提供图片的静态访问方法
@@ -33,9 +27,8 @@ public class ImageManager {
      * 可使用 CLASSNAME_IMAGE_MAP.get( obj.getClass().getName() ) 获得 obj 所属基类对应的图片
      */
     private static final Map<String, Bitmap> CLASSNAME_IMAGE_MAP = new HashMap<>();
-
-    public static Bitmap BACKGROUND_IMAGE;
-    public static Bitmap HERO_IMAGE;
+    public  static Bitmap BACKGROUND_IMAGE;
+    public static   Bitmap HERO_IMAGE;
     public static Bitmap HERO_BULLET_IMAGE;
     public static Bitmap ENEMY_BULLET_IMAGE;
     public static Bitmap MOB_ENEMY_IMAGE;
@@ -45,9 +38,8 @@ public class ImageManager {
     public static Bitmap BOMB_PROP_IMAGE;
     public static Bitmap FIRE_PROP_IMAGE;
 
-
-    static {
-            CLASSNAME_IMAGE_MAP.put(HeroAircraft.class.getName(), HERO_IMAGE);
+     public static void updateAll(){
+        CLASSNAME_IMAGE_MAP.put(HeroAircraft.class.getName(), HERO_IMAGE);
             CLASSNAME_IMAGE_MAP.put(MobEnemy.class.getName(), MOB_ENEMY_IMAGE);
             CLASSNAME_IMAGE_MAP.put(HeroBullet.class.getName(), HERO_BULLET_IMAGE);
             CLASSNAME_IMAGE_MAP.put(EnemyBullet.class.getName(), ENEMY_BULLET_IMAGE);
@@ -58,15 +50,8 @@ public class ImageManager {
             CLASSNAME_IMAGE_MAP.put(FireProp.class.getName(), FIRE_PROP_IMAGE);
     }
 
-    public static Bitmap get(String className){
-        return CLASSNAME_IMAGE_MAP.get(className);
-    }
-
-    public static Bitmap get(Object obj){
-        if (obj == null){
-            return null;
-        }
-        return get(obj.getClass().getName());
+    public  static Bitmap get(Object obj){
+        return CLASSNAME_IMAGE_MAP.get(obj.getClass().getName());
     }
 
 }
